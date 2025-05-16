d30c52fff7fd88ab36769c87027ba23aed7e60ac
Kayode <kayodeegunjobi44@gmail.com>
2025-05-16 08:46:43 +0100

    git push -u origin main

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -32,11 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if ENVIRONMENT == 'development':
-    DEBUG = True
-else:
-    
-    DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = ['artgallery-yyxu.onrender.com', 'localhost', '127.0.0.1']
 
